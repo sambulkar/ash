ac5a208be7824853a12e0f300d02810d38cabb02
sambulkar <sachin.ambulkar@gmail.com>
2020-03-16 10:22:46 +0800

    Update jenkinsinstall.sh

--- jenkinsinsatll/jenkinsinstall.sh
+++ jenkinsinsatll/jenkinsinstall.sh
@@ -1,6 +1,4 @@
 
-#JENKINS URL http://18.140.246.208:8080/
-# JENKINS INSTALLATION AND CONFIGURATION SERVER
 #Installing JAVA on JENKINS SERVER
 sudo yum update -y
 sudo yum install java-1.8.0-openjdk-devel
@@ -16,7 +14,6 @@ sudo yum install jenkins -y
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
 sudo systemctl status jenkins
-sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 
 #Install Ansible on Jenkins
 sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
@@ -25,13 +22,6 @@ sudo yum -y install ansible
 #Install git client on Jenkins server
 sudo yum install git
 
-
-# CREATE REQUIRED DIR ON JENKINS SERVER FOR ANSIBLE
-cd /home/ec2-user
-mkdir ansible
-cd ansible/
-mkdir .keys
-chmod 400 demo.pem
 
 
 
